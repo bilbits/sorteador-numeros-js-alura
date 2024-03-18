39fd0a53bb26f0538a761fa52c9c8609e317e593
Gabriel Ribeiro <Gabrielribeiro.dev23@gmail.com>
2024-03-18 12:20:54 -0300

    Adicionada a função de sorteio de números com uma mensagem de alerta informando a quantidade de números sorteados, a partir de qual número vai iniciar o sorteio e em qual núnero o sorteio acaba

---
+++ app.js
@@ -0,0 +1,9 @@
+function sortear () {
+    let quantidade = document.getElementById('quantidade').value;
+let doNumero = document.getElementById('de').value;
+let ateNumero = document.getElementById('ate').value;
+
+alert(`Quantidade de números: ${quantidade}`);
+alert(`Do número: ${doNumero}`);
+alert(`Até o número: ${ateNumero}`);
+}
