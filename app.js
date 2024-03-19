7b72e9fcab806e1c1e4ee5805f795f740041dce3
Gabriel Ribeiro <Gabrielribeiro.dev23@gmail.com>
2024-03-19 11:05:30 -0300

    Agora os números sorteados serão exibidos dentro da página html

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ for (let i = 0; i < quantidade; i++) {
 numero = obterNumeroAleatorio(doNumero, ateNumero);
 sorteados.push(numero);
 }
-alert(sorteados);
+let exibirSorteados = document.getElementById('resultado');
+exibirSorteados.innerHTML = `<label class ="texto__paragrafo"> Números sorteados até agora: ${sorteados}</label>`
 }  
 
 function obterNumeroAleatorio(min, max) {
